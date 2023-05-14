b32048b26a845378bd3a3da4d926835504899ef1
dinhson98 <Dinhsonmta55@gmail.com>
2023-05-14 15:08:55 +0700

    up

---
+++ Bai4_Lop_va_Doi_Tuong_trong_Java/src/xay_dung_lop_chi_ghi_trong_java/StudenTest.java
@@ -0,0 +1,11 @@
+package xay_dung_lop_chi_ghi_trong_java;
+
+public class StudenTest {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Alice");
+        student.setClasses("C01");
+        System.out.println(student.getName());
+        System.out.println(student.getClass());
+    }
+}
