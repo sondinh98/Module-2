b17c3e054651f568437eed9115b727776573a71a
dinhson98 <Dinhsonmta55@gmail.com>
2023-05-18 15:13:21 +0700

    up

---
+++ Bai11/task2/src/LinkedListQueueClient.java
@@ -0,0 +1,19 @@
+public class LinkedListQueueClient {
+    public static void main(String[] args) {
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(10);
+        System.out.println("Dequeued item is " + queue.dequeue().key);
+
+        queue.enqueue(20);
+        System.out.println("Dequeued item is " + queue.dequeue().key);
+        System.out.println("Queue after dequeue");
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        System.out.println("Dequeued item is " + queue.dequeue().key);
+        System.out.println("Dequeued item is " + queue.dequeue().key);
+        System.out.println("Dequeued item is " + queue.dequeue().key);
+    }
+}
