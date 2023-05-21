ab394b90fe5c33988ac8810e4e5c895be04e086d
dinhson98 <Dinhsonmta55@gmail.com>
2023-05-21 12:51:19 +0700

    up

---
+++ Bai12/task7/src/BST.java
@@ -0,0 +1,56 @@
+public class BST {
+    Node root;
+
+    public BST() {
+        root = null;
+    }
+
+    public void deleteKey(int key) {
+        root = deleteRec(root, key);
+    }
+
+    private Node deleteRec(Node root, int key) {
+        if (root == null) {
+            return root;
+        }
+
+        if (key < root.key) {
+            root.left = deleteRec(root.left, key);
+        } else if (key > root.key) {
+            root.right = deleteRec(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            root.key = minValue(root.right);
+
+            root.right = deleteRec(root.right, root.key);
+        }
+
+        return root;
+    }
+
+    private int minValue(Node root) {
+        int minv = root.key;
+        while (root.left != null) {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    public void inorder() {
+        inorderRec(root);
+    }
+
+    private void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.key + " ");
+            inorderRec(root.right);
+        }
+    }
+}
---
+++ Bai12/task7/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        BST tree = new BST();
+
+        /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+        tree.root = new Node(50);
+        tree.root.left = new Node(30);
+        tree.root.right = new Node(70);
+        tree.root.left.left = new Node(20);
+        tree.root.left.right = new Node(40);
+        tree.root.right.left = new Node(60);
+        tree.root.right.right = new Node(80);
+
+        System.out.println("Inorder traversal of the given tree");
+        tree.inorder();
+
+        System.out.println("\nDelete 20");
+        tree.deleteKey(20);
+        System.out.println("Inorder traversal of the modified tree");
+        tree.inorder();
+
+        System.out.println("\nDelete 30");
+        tree.deleteKey(30);
+        System.out.println("Inorder traversal of the modified tree");
+        tree.inorder();
+
+        System.out.println("\nDelete 50");
+        tree.deleteKey(50);
+        System.out.println("Inorder traversal of the modified tree");
+        tree.inorder();
+    }
+}
+
