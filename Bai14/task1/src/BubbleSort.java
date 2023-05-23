4a23cdb45f3f8eebcd895b4d114b62aed199dac5
dinhson98 <Dinhsonmta55@gmail.com>
2023-05-23 14:39:58 +0700

    up

--- Bai14/task1/src/BubbleSort.java
+++ Bai14/task1/src/BubbleSort.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class BubbleSort {
+public class BubbleSort {
+    static int[] list = {16, 3, 25, 10, 5};
+
+    public static void bubbleSort(int[] list) {
+        boolean needNextPass = true;
+        for (int i = 1; i < list.length & needNextPass; i++) {
+            needNextPass = false;
+            for (int j = 0; j < list.length - i; j++) {
+                if (list[j] > list[j + 1]) {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i] + " ");
+        }
+    }
 }
