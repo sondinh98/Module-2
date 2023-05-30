62e7f914f6f82b21ab43fc8e75609b4f57df6675
dinhson98 <Dinhsonmta55@gmail.com>
2023-05-30 21:21:51 +0700

    up

---
+++ Bai18/task4/src/NumberGenerator.java
@@ -0,0 +1,13 @@
+public class NumberGenerator implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Thread " + Thread.currentThread().hashCode() + ": " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
